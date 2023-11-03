c91bab4ad45b8f8f617cb6c763139238559f32d5
Zombie <73972020+Shiro4k@users.noreply.github.com>
2023-11-03 18:40:38 +0800

    add README

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string GetArr(string message)
+{
+    Console.Write($"{message}");
+    string num = Console.ReadLine();
+    return num;
+}
+int GetNum(string message)
+{
+    Console.Write($"{message}");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+string[] CreateArray()
+{
+    Console.Write("Введите размер массива: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[size];
+    for (int i = 0; i < size; i += 1)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+
+
+
+
+int Arrlenght = GetNum("Введите размер масива: ");
+string[] arr = new string[Arrlenght];
